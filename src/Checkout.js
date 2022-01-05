a23721e398196b9f08b7b0392477fb9c05f58398
Deshant Singh <singhdesh29@gmail.com>
2022-01-05 19:05:17 +1200

    Checkout-Total

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import  './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
 import Subtotal from './Subtotal';
-
+import {useStateValue} from './StateProvider'
 
 
 function Checkout() {
+    const [{basket},dispatch] = useStateValue();
+
     return (
         <div className='checkout'>
             
@@ -12,6 +15,17 @@ function Checkout() {
             <img src='https://www.wordstream.com/wp-content/uploads/2021/07/banner-ads-examples-aws.jpg.webp' alt=''/>
                 <div>
                     <h2 className='checkout__title'>your shopping basket</h2>
+
+                    {basket.map(item => (
+                        <CheckoutProduct
+                            id={item.id}
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}
+                            />
+                    ))}
+
                 </div>
             </div>
             <div className='checkout__right'>
